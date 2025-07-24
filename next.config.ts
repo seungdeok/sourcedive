ef550d95918f590291ca067fe32e57bac3c0fa88
seungdeok <akk9132@naver.com>
2025-07-24 18:11:06 +0900

    chore: exclude option 환경 변경

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   compiler: {
     removeConsole: {
-      exclude: ["error", "warn"],
+      exclude: process.env.NODE_ENV === "production" ? ["error", "warn"] : ["error", "warn", "log"],
     },
   },
   experimental: {
