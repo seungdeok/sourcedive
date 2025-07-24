c60d1c4826ea8d2d9ed7d19581f3555dcc585761
seungdeok <akk9132@naver.com>
2025-07-24 20:17:25 +0900

    refactor: playwright webserver 설정

--- __test__/e2e/example.spec.tsx
+++ __test__/e2e/example.spec.tsx
@@ -1,7 +1,7 @@
 import { expect, test } from "@playwright/test";
 
 test("should navigate to the about page", async ({ page }) => {
-  await page.goto("http://localhost:3000/");
-  await expect(page).toHaveURL("http://localhost:3000");
+  await page.goto("/");
+  await expect(page).toHaveURL("/");
   await expect(page.locator("h1")).toContainText("Source Dive");
 });
--- playwright.config.ts
+++ playwright.config.ts
@@ -2,6 +2,17 @@ import { defineConfig, devices } from "@playwright/test";
 
 export default defineConfig({
   // Opt out of parallel tests on CI.
+  use: {
+    baseURL: "http://localhost:3000",
+  },
   testDir: "__test__/e2e",
   workers: process.env.CI ? 1 : undefined,
+  webServer: {
+    command: "pnpm run dev",
+    url: "http://localhost:3000",
+    reuseExistingServer: !process.env.CI,
+    stdout: "ignore",
+    stderr: "pipe",
+    timeout: 120000,
+  },
 });
