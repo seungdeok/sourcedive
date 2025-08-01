d56f600be4c19e5a8a377f17b1bcd5c5882a26b5
seungdeok <akk9132@naver.com>
2025-08-01 22:32:25 +0900

    feat: @vercel/speed-insights 추가

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import Header from "@/components/Header";
 import { Analytics } from "@vercel/analytics/next";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
@@ -27,6 +28,7 @@ export default function RootLayout({
         <Header />
         <main className="container mx-auto pt-12">{children}</main>
         {process.env.NODE_ENV === "production" && <Analytics />}
+        {process.env.NODE_ENV === "production" && <SpeedInsights />}
       </body>
     </html>
   );
