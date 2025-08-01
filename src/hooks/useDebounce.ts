7bff7b3e69f2413c4295509ebc0654525abb2626
seungdeok <akk9132@naver.com>
2025-08-02 01:39:19 +0900

    feat: 추천 검색어 추가

---
+++ src/hooks/useDebounce.ts
@@ -0,0 +1,24 @@
+import { useEffect, useRef, useState } from "react";
+
+export function useDebounce<T>(value: T, delay = 200) {
+  const [debouncedValue, setDebouncedValue] = useState<T>(value);
+  const timeoutRef = useRef<NodeJS.Timeout | null>(null);
+
+  useEffect(() => {
+    if (timeoutRef.current) {
+      clearTimeout(timeoutRef.current);
+    }
+
+    timeoutRef.current = setTimeout(() => {
+      setDebouncedValue(value);
+    }, delay);
+
+    return () => {
+      if (timeoutRef.current) {
+        clearTimeout(timeoutRef.current);
+      }
+    };
+  }, [value, delay]);
+
+  return debouncedValue;
+}
