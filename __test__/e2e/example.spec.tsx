2bb2d092a246c7124b8298e5967971ffc0463961
seungdeok <akk9132@naver.com>
2025-08-02 09:17:59 +0900

    refactor: header icon 수정

--- __test__/e2e/example.spec.tsx
+++
@@ -1,7 +0,0 @@
-import { expect, test } from "@playwright/test";
-
-test("should navigate to the about page", async ({ page }) => {
-  await page.goto("/");
-  await expect(page).toHaveURL("/");
-  await expect(page.locator("h1")).toContainText("Source Dive");
-});
