807ad632defdebf2a55b2e16f1d10290ffee4dcc
seungdeok <akk9132@naver.com>
2025-08-02 09:18:17 +0900

    refactor: favicon 추가

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -24,6 +24,11 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="ko">
+      <head>
+        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+      </head>
       <body className={`${pretendard.variable} antialiased min-h-screen`}>
         <Header />
         <main className="container mx-auto pt-12 px-4 sm:px-6 lg:px-8">{children}</main>
